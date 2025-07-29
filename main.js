ccbcb8f676e4f4b89b39751f5f00b18d4a388872
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-07-29 16:01:49 +0100

    more js functionality

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-g5
+"use strict"
+
+const toList = []
+
+const form = document.getElementById("myForm");
+const mySection = document.getElementById("listSection"); 
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    const formInput = document.getElementById("text");
+    const formValue = formInput.value.trim();
+
+    if (formValue.length !== 0){
+        toList.push(formValue);
+        if(formValue){
+            const para = document.createElement("p");
+            para.textContent = `${formValue}`;
+            mySection.appendChild(para);
+
+        }
+    }
+
+    else if (formValue.length === 0) {
+        alert("you have not type any to do list");
+    };
+
+});
+
+
+
+
